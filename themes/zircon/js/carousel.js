407df771751264d8732b51e90b0f26cc3d20f166
amanpathak05 <pathakaman05@gmail.com>
2016-02-22 20:27:21 +0530

    New Changes

--- themes/zircon/js/carousel.js
+++ themes/zircon/js/carousel.js
@@ -4,13 +4,14 @@
   Drupal.behaviors.zirconCarousel = {
     attach: function (context, settings) {  
       $(document).ready(function() {
+		  //----- Photo Gallery -----------
       if($('.carousel-responsive').length) {
         $('.carousel-responsive').slick({
             dots: false,
             infinite: true,
             speed: 300,
             slidesToShow: 4,
-            slidesToScroll: 4,
+            slidesToScroll: 1,
             responsive: [{
                 breakpoint: 992,
                 settings: {
@@ -34,6 +35,40 @@
             }]
           });
         }
+		
+		//-------- Video Library -------
+      if($('.video-carousel-responsive').length) {
+        $('.video-carousel-responsive').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            }, {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+          });
+        }
+		
+		
       });
     }
   };
